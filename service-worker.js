2556c4b2fdfd1ecd2645b5457b38b157da7ab572
dika-81 <andikaarisetyawan@upi.edu>
2026-07-09 14:58:48 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'congklak-fpb-kpk-v1';
+const BASE_PATH = '/congklak-fpb-kpk/';
+
+const APP_SHELL = [
+  BASE_PATH,
+  BASE_PATH + 'index.html',
+  BASE_PATH + 'manifest.webmanifest',
+  BASE_PATH + 'offline.html',
+  BASE_PATH + 'icons/icon-192.png',
+  BASE_PATH + 'icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) return cachedResponse;
+
+      return fetch(event.request)
+        .then((networkResponse) => {
+          const cloned = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, cloned));
+          return networkResponse;
+        })
+        .catch(() => {
+          if (event.request.mode === 'navigate') {
+            return caches.match(BASE_PATH + 'offline.html');
+          }
+        });
+    })
+  );
+});
